2e17165e591e4a27c3c44730e6664aac84ee4dc4
John Norwood <norwood.john.m@gmail.com>
2017-08-23 00:17:17 -0500

    [SC-15] Refactored a bit to use the new web service chart I wrote in another project, also added a readinessProbe and a livenessProbe

--- server/packages/stupidchess/src/com/johnmalcolmnorwood/stupidchess/app.py
+++ server/packages/stupidchess/src/com/johnmalcolmnorwood/stupidchess/app.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 from flask import Flask, jsonify
 from flask_mongoengine import MongoEngine
+from mongoengine.connection import get_db
 
 from com.johnmalcolmnorwood.stupidchess.blueprints.game_blueprint import game_blueprint
 from com.johnmalcolmnorwood.stupidchess.exceptions import IllegalMoveException, InvalidGameParameterException
@@ -11,13 +12,13 @@ from com.johnmalcolmnorwood.auth.initialize_authentication import initialize_aut
 app = Flask(__name__)
 app.context = ApplicationContext()
 
-initialize_authentication(app, auth_url_prefix='/api/user')
+initialize_authentication(app, auth_url_prefix="/api/user")
+app.register_blueprint(game_blueprint, url_prefix="/api/game")
 
-app.register_blueprint(game_blueprint, url_prefix='/api/game')
 
-app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongo',
-    'db': 'stupidchess'
+app.config["MONGODB_SETTINGS"] = {
+    "host": "mongo",
+    "db": "stupidchess"
 }
 
 MongoEngine(app)
@@ -26,11 +27,18 @@ MongoEngine(app)
 @app.errorhandler(IllegalMoveException)
 def handle_invalid_usage(error):
     return jsonify(
-        message='Failed to apply illegal move',
-        move=error.move.to_dict('startSquare', 'destinationSquare', 'type'),
+        message="Failed to apply illegal move",
+        move=error.move.to_dict("startSquare", "destinationSquare", "type"),
     ), 400
 
 
 @app.errorhandler(InvalidGameParameterException)
 def handle_invalid_usage(error):
     return jsonify(message=error.message), 400
+
+
+@app.route("/api/health")
+def deep_health():
+    db = get_db()
+    stats = db.command("dbstats")
+    return jsonify({"ok": stats["ok"]}), 200 if stats["ok"] else 500

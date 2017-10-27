57a2edc59a46f6b9054e16db8e79bf56468dfd5d
John Norwood <norwood.john.m@gmail.com>
2017-10-27 11:32:51 -0500

    [RELEASE] Bumping versions to release version 17.1028.0

--- server/packages/auth/setup.py
+++ server/packages/auth/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="com.johnmalcolmnorwood.auth",
-    version="17.1017.0-dev",
+    version="17.1028.0",
     package_dir={"": "src"},
     packages=find_packages("src"),
     zip_safe=False,

3a8433453790941fcc5e73ae2e5082ed8d45b0e0
John Norwood <norwood.john.m@gmail.com>
2023-10-16 19:31:38 +0100

    Release version 2023.10.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stupidchess",
-    version="2023.10.0",
+    version="2023.10.1",
     packages=find_packages(),
     zip_safe=False,
     package_data={
